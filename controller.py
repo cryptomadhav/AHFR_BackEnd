d663283c4392f399a0a45568505abef1fc87e5bf
cryptomadhav <cryptomadhav@gmail.com>
2020-11-01 17:50:13 +0530

    pip requirements file added

--- controller.py
+++ controller.py
@@ -3,9 +3,13 @@ from flask import Flask, jsonify, request
 app= Flask("__name__")
 @app.route("/", methods=["GET"])
 def message():
+    return jsonify("Service is live!! Hit /img_to_json to process image!!")
+
+@app.route("/img_to_json", methods=["GET"])
+def img_to_json():
     posted_data = request.get_json()
     name = posted_data['name']
-    return jsonify(" Hope you are having a good time " +  name + "!!!")
+    return jsonify("Hope you are having a good time " +  name + "!!!")
 #  main thread of execution to start the server
 if __name__=='__main__':
     app.run(debug=True)
